140eeb7147be4d270036b29149a7fe66cc5d8a30
eve <eve@localhost.localdomain>
2017-02-13 23:12:31 +0100

    Fix invalid memory pointer

--- src/middleware.c
+++ src/middleware.c
@@ -23,70 +23,71 @@
 
 middleware(auth_basic)
 {
-	if (!request->hdlr_extra)
+    if (!request->hdlr_extra)
         request->hdlr_extra = kore_calloc(1, sizeof(struct request_data));
 
-	/* Protect route with basic authentication */
-	if (!http_basic_auth(request, STATUSPAGE_AUTH)) {
-		size_t len;
-		char *report = http_report(401, "Authorization required", &len);
+    /* Protect route with basic authentication */
+    if (!http_basic_auth(request, STATUSPAGE_AUTH)) {
+        size_t len;
+        char *report = http_report(401, "Authorization required", &len);
 
-		http_response_header(request, "www-authenticate", "Basic realm=\"Baisc auth\"");
-		http_response_header(request, "content-type", "text/html");
-		http_response(request, 401, report, len);
-		kore_free(report);
-		return_error();
-	}
+        http_response_header(request, "www-authenticate", "Basic realm=\"Baisc auth\"");
+        http_response_header(request, "content-type", "text/html");
+        http_response(request, 401, report, len);
+        kore_free(report);
+        return_error();
+    }
 
-	/* Find user part */
-	char *principal = kore_strdup(STATUSPAGE_AUTH);
-	char *split = strchr(principal, ':');
-	split[0] = '\0';
+    /* Find user part */
+    char *principal = kore_strdup(STATUSPAGE_AUTH);
+    char *split = strchr(principal, ':');
+    split[0] = '\0';
 
-	//TODO: Free internals
-	struct request_data *auth = (struct request_data *)request->hdlr_extra;
-	auth->auth.object_id = 1;
-	auth->auth.principal = principal;
+    //TODO: Free internals
+    struct request_data *auth = (struct request_data *)request->hdlr_extra;
+    auth->auth.object_id = 1;
+    auth->auth.principal = principal;
 
     return_ok();
 }
 
 middleware(auth_jwt)
 {
-	struct jwt jwt;
+    struct jwt jwt;
 
-	if (!request->hdlr_extra)
+    memset(&jwt, '\0', sizeof(struct jwt));
+    if (!request->hdlr_extra)
         request->hdlr_extra = kore_calloc(1, sizeof(struct request_data));
 
     char *method = strtok(data, " ");
     if (!method)
         return_error();
 
-	/* Require bearer authentication */
+    /* Require bearer authentication */
     if (!strcmp(strtolower(method), "bearer")) {
         char *token = strtok(NULL, " ");
 
-		/* Verify token */
-		if (jwt_verify(token, &jwt)) {
+        /* Verify token */
+        if (jwt_verify(token, &jwt)) {
 
-			//TODO: Free internals
+            //TODO: Free internals
             struct request_data *auth = (struct request_data *)request->hdlr_extra;
-			auth->auth.object_id = jwt.oid;
-			auth->auth.principal = kore_strdup(jwt.sub);
+            auth->auth.object_id = jwt.oid;
+            auth->auth.principal = kore_strdup(jwt.sub);
 
-			/* Cleanup */
-			kore_free(jwt.iss);
-			kore_free(jwt.sub);
-			kore_free(jwt.aud);
+            /* Cleanup */
+            kore_free(jwt.iss);
+            kore_free(jwt.sub);
+            kore_free(jwt.aud);
 
-			return_ok();
-		}
+            return_ok();
+        }
 
-		/* Cleanup */
-		kore_free(jwt.iss);
-		kore_free(jwt.sub);
-		kore_free(jwt.aud);
+        /* Cleanup */
+        kore_free(jwt.iss);
+        kore_free(jwt.sub);
+        kore_free(jwt.aud);
     }
 
-	return_error();
+    return_error();
 }
