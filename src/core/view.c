b4614cd3365070c8a695529b6cdf07eadbeb156c
yorickdewid <yorick17@outlook.com>
2017-02-19 00:38:53 +0100

    Call functions

--- src/core/view.c
+++ src/core/view.c
@@ -56,12 +56,32 @@ replace_string(struct kore_buf *b, char *src, void *dst, size_t len)
     }
 }
 
+static char **
+split_arguments(char *cmdline, size_t cmdlinesz, int *argc)
+{
+    char **argv = (char **)kore_calloc(16, sizeof(char *));
+    *argc = 0;
+
+    size_t i; char *next = cmdline;
+    for (i=0; i<cmdlinesz && *argc < 16; ++i) {
+        if (cmdline[i] == ',') {
+            cmdline[i++] = '\0';
+
+            /* Arguments */
+            argv[(*argc)++] = next;
+            next = cmdline + i;
+        }
+    }
+
+    return argv;
+}
+
 static char *
-include_asset(const char *name, size_t namesz)
+include_asset(const char *name)
 {
     char buffer[64];
 
-    snprintf(buffer, 64, "asset_%.*s_html", (int)namesz, name);
+    snprintf(buffer, 64, "asset_%s_html", name);
     puts(buffer);
 
     void *hndl = dlopen(NULL, RTLD_LAZY);
@@ -73,11 +93,36 @@ include_asset(const char *name, size_t namesz)
     char *asset = dlsym(hndl, buffer);
     if (!asset) {
         puts("dlsym");
+        dlclose(hndl);
         return NULL;
     }
 
     dlclose(hndl);
     return asset;
+}
+
+static void
+call_vfunc(int argc, char *argv[])
+{
+    typedef char *(*decl_vfunc)(int argc, char *argv[]);
+    decl_vfunc vfunc;
+
+    void *hndl = dlopen(NULL, RTLD_LAZY);
+    if (!hndl) {
+        puts("dlopen");
+        return;
+    }
+
+    *(char **)(&vfunc) = dlsym(hndl, argv[0]);
+    if (!vfunc) {
+        puts("dlsym");
+        dlclose(hndl);
+        return;
+    }
+
+    vfunc(argc, argv);
+
+    dlclose(hndl);
 }
 
 static char *
@@ -115,11 +160,48 @@ process_include(struct kore_buf *buffer)
         if (!argument || !argumentsz)
             return;
 
-        char *asset = include_asset(argument, argumentsz);
+        argument[argumentsz] = '\0';
+        char *asset = include_asset(argument);
         if (!asset)
             return;
 
-        replace_string(buffer, "@include(body)", asset, strlen(asset));
+        replace_string(buffer, "@include(body", asset, strlen(asset));//TODO: replace between offsets
+    //}
+}
+
+char *foo_bar(int argc, char *argv[]);
+char *
+foo_bar(int argc, char *argv[])
+{
+    puts("JUPS");
+    return "kaas";
+}
+
+static void
+process_function(struct kore_buf *buffer)
+{
+    char            *argument;
+    size_t          argumentsz;
+
+    //for (;;) {
+        argumentsz = 0;
+        argument = parser(buffer, TOk_FUNCTION, sizeof(TOk_FUNCTION) - 1, &argumentsz);
+        if (!argument || !argumentsz)
+            return;
+
+        argument[argumentsz] = '\0';
+        printf("Execute: %s\n", argument);
+
+        int argc;
+        char **argv = split_arguments(argument, argumentsz, &argc);
+        if (!argc)
+            return;
+
+        call_vfunc(argc, argv);
+
+        kore_free(argv);
+
+        // replace_string(buffer, "@function(get_server_name, 3, 5)", asset, strlen(asset));
     //}
 }
 
@@ -151,6 +233,7 @@ view(struct http_request *request, const char *view)
         /* Parse view in order */
         process_include(buffer);
         process_vars(buffer);
+        process_function(buffer);
     }
 
     /* Convert and respond */
